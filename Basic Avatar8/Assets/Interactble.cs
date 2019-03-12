a9bd92ea12b026318eb5ce8f8d97e737d8ba8bd9
Unknown <jack2.lynham@live.uwe.ac.uk>
2019-03-12 17:02:19 +0000

    Started Interaction

---
+++ Basic Avatar8/Assets/Interactble.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Interactble : MonoBehaviour
+{
+    public float radius = 3f;
+
+  void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
