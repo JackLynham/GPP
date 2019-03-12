294fc79d5a48032fe1bc8edd10823b7fbba1d46d
Unknown <jack2.lynham@live.uwe.ac.uk>
2019-03-12 14:52:13 +0000

    Basic Ai Implemented

---
+++ Basic Avatar8/Assets/AiController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AiController : MonoBehaviour
+{
+    public float lookRaidus = 10f;
+
+    Transform target;
+     NavMeshAgent agent;
+	// Use this for initialization
+	void Start ()
+    {
+        target = TrackPlayer.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+       
+    
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+        
+        if(distance <= lookRaidus)
+        {
+            agent.SetDestination(target.position);
+        }
+	}
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRaidus);
+        
+    }
+}
---
+++ Basic Avatar8/Assets/TrackPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackPlayer : MonoBehaviour {
+
+    // Use this for initialization
+    #region Singleton
+
+    public static TrackPlayer instance;
+ void Awake()
+    {
+        instance = this;
+
+    }
+
+    #endregion
+    public GameObject player;
+   
+}
