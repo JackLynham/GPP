32e2da361f7b31fe555c494b9134f1f33e9e0d0b
Unknown <jack2.lynham@live.uwe.ac.uk>
2019-03-14 19:41:25 +0000

    Started CHanges

---
+++ Basic Avatar8/Assets/New Scene Stuff/Scripts/Interactble.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Interactble : MonoBehaviour
+{
+    public float radius = 3f;
+    bool isFocus = false;
+    Transform player;
+    void Update()
+    {
+       // Debug.Log(player);
+        if (isFocus)
+        {
+            float distance = Vector3.Distance(player.position, transform.position);
+            if (distance <= radius)
+            {
+               // Debug.Log("Interact");
+                
+            }
+        }
+    }
+    public void Onfocused(Transform playerTransform)
+    {
+        isFocus = true;
+        player = playerTransform;
+    }
+
+    public void OnDeFocused()
+    {
+        isFocus = false;
+        Debug.Log("A");
+        player = null;
+    }
+
+  void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
---
+++ Basic Avatar8/Assets/New Scene Stuff/Scripts/PlayerController.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    public LayerMask movementMask;
+    Camera cam;
+     public PlayerMotor motor;
+    public float moveSpeed = 10f;
+    public float turnSpeed = 150f;
+
+    public Interactble focus;
+    public Interactble bgh;
+
+    // Use this for initialization
+    void Start()
+    {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movement
+        if (Input.GetAxisRaw("Vertical") == 1)
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            //motor.StopFollowingTarget();
+            bgh.OnDeFocused();
+        }
+        if (Input.GetAxisRaw("Vertical") == -1)
+        {
+            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+            motor.StopFollowingTarget();
+        }
+
+        if (Input.GetAxisRaw("Horizontal") == -1)
+        {
+            transform.Rotate(Vector3.down * -turnSpeed * Time.deltaTime);
+
+
+        }
+
+        if (Input.GetAxisRaw("Horizontal") == 1)
+        {
+            transform.Rotate(Vector3.up * -turnSpeed * Time.deltaTime);
+        }
+        
+        //Collecting Stuff
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Interactble intera = hit.collider.GetComponent<Interactble>();
+                //Foucs on interactble
+                if (intera != null)
+                {
+                    SetFocus(intera);
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                RemoveFocus();
+            }
+        }
+    }
+
+    public void SetFocus(Interactble newFocus)
+    {
+        if (focus!= null)
+            {
+             focus.OnDeFocused();
+            }
+        if(newFocus!= focus)
+        {
+           
+      
+            focus = newFocus;
+            motor.FollowTarget(newFocus);
+        }
+   
+        newFocus.Onfocused(transform);
+       
+    }
+    public void RemoveFocus()
+    {
+
+        focus = null;
+        motor.StopFollowingTarget();
+       
+
+        if (focus != null)
+        {
+            focus.OnDeFocused();
+        }
+    }
+
+    
+}
